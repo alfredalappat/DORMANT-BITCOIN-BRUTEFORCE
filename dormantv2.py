07af8f616dc6775f88fdae596b5f52dfa19b1715
alfredalappat <163707179+alfredalappat@users.noreply.github.com>
2026-09-09 00:40:59 +0530

    Add files via upload

---
+++ dormantv2.py
@@ -0,0 +1,50 @@
+import time,os
+from bit import Key
+from bit.format import bytes_to_wif
+
+
+
+output_file = "found.txt"
+st=int(time.time())
+hex_count = 0
+
+
+with open("addresses.txt", "r", encoding="utf-8") as f:
+    addresses = {line.strip() for line in f if line.strip()}
+
+
+print("Total unique addresses:", len(addresses))
+def generate_bitcoin_addresses():
+    # Generate random 256-bit private key (as hex)
+    priv_bytes = os.urandom(32)
+    c=priv_bytes.hex()
+    
+    # Create compressed key
+    wif_compressed = bytes_to_wif(priv_bytes, compressed=True)
+    key_compressed = Key(wif_compressed)
+
+    # Create uncompressed key
+    wif_uncompressed = bytes_to_wif(priv_bytes, compressed=False)
+    key_uncompressed = Key(wif_uncompressed)
+
+    checking(c,key_compressed.address)
+    checking(c,key_uncompressed.address)
+
+
+def checking(privateKey,addr):
+    if addr in addresses:
+            print(f'\nPrivate Key: {privateKey}, Address: {addr}')
+            result_string = f'Private Key: {privateKey}, Address: {addr}\n'
+            with open(output_file, 'a') as f:
+                f.write(result_string)
+
+
+
+
+while True:
+    generate_bitcoin_addresses()
+    hex_count += 1
+    if hex_count%1000000==0:
+        en=int(time.time())
+        print(hex_count,f"speed = {int(hex_count/(en-st))} per sec")
+    
